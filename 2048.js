c63fbd240d01f05661ca6e4c01a8e92ec4c46ace
John Kosmicke <kozjob@yahoo.com>
2019-04-23 07:58:59 -0700

    first

---
+++ 2048.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready( function() {
+  controller2048.init();
+});
+
+
+
---
+++ controller.js
@@ -0,0 +1,13 @@
+
+controller2048 = {
+
+  model: model2048,
+  view: view2048,
+
+  init: function(  ){
+    this.model.init();
+    this.view.init();
+  },
+
+};
+
